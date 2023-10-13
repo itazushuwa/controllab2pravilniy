a229bf653d9e8c7299c10bdf07636e265d2afac9
SuvorovDA <SuvorovDA@310D-4>
2023-10-13 15:31:12 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SumSeriesLib1;
+
+class Program
+{
+    static void Main()
+    {
+        double x = 0.5;
+        double eps = 0.001;
+
+        Calculation calculation = new Calculation();
+        List<double> seriesValues = calculation.CalculateSumSeries(x, eps);
+
+        foreach (double value in seriesValues)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
